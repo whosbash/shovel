35a090fdfae55ac9aca2f9e588879eb896b624bc
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-09 18:23:48 -0300

    feat/ cities, locations and data

--- scripts/cities_location.sh
+++ scripts/cities_location.sh
@@ -15,7 +15,9 @@ function extractGeoCoordinate() {
 	fname="city.html"
 	URL="https://en.wikipedia.org/wiki/$1"
 
-	xmlstartlet_pattern='(//span[@class="latitude"]/text())[position()=1] | (//span[@class="longitude"]/text())[position()=1]'
+	pattern_1='(//span[@class="latitude"]/text())[position()=1]'
+	pattern_2='(//span[@class="longitude"]/text())[position()=1]'
+	xmlstartlet_pattern="$pattern_1 | $pattern_2"
 	perl_pattern='s|^(\d+)\D+(\d+)\D+(\d+).*|$1+($2/60)+($3/60)/60|e'
 
 	curl -s $URL > "$fname"
@@ -26,9 +28,32 @@ function extractGeoCoordinate() {
 	echo "$(convertDMStoDecimal "$lat_lng")"
 }
 
-extractGeoCoordinate $1
+cities_filename=$1
 
+to_csv_filename="BR_tmp.csv"
+rm -f "$to_csv_filename" && touch "$to_csv_filename"
 
+cat "$cities_filename" | \
+tail -n +2 | 
+awk '{ print $1 }' FS="," | \
+perl -lne 'print $2 while m{(["'\''])((?:\\.|(?!\1).)*+)\1}g' | \
+sed 's/ /_/g' | \
+uniq >> "$to_csv_filename"
 
+to_output_filename='cities_lat_lng.csv'
+rm -f "$to_output_filename" && touch "$to_output_filename"
 
+line_count="$(wc -l < $to_csv_filename)"
+i=1
+perc=0
+scale=7
+while read city; do	
+	lat_lng="$(extractGeoCoordinate "$city" | sed 's/ /,/g')"
+	echo "$city,$lat_lng" >> $to_output_filename
 
+	perc="$(echo "scale=$scale; $i/$line_count" | bc -l)"
+	echo "$perc% ($i/$line_count): $city,$lat_lng"
+	i="$(echo "$i+1" | bc)"
+done < "$to_csv_filename"
+
+rm -f BR_tmp.csv
